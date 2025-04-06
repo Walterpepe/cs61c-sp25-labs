5b843094d1c76cbea340b0d057f7ebb8cb6c1793
liyu <1271946196@qq.com>
2025-04-06 18:24:11 +0800

    lab03

--- lab03/ex5_factorial.s
+++ lab03/ex5_factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 7
 
 .text
 # Don't worry about understanding the code in main
@@ -27,7 +27,17 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
+    mv t0, a0       # n
+    addi t1, x0, 1  # product
+    addi t2, x0, 2  # limit
+loop:
+    blt t0, t2, finish # exit loop if t0 < 2
+    mul t1, t1, t0 # product = product * n
+    addi t0, t0, -1 # decrement n
+    j loop # loop
 
+finish:
+    mv a0, t1 # return value
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
